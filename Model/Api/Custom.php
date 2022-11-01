50e3d0884ddb2699bc2ef1442cf44023698793ef
Fabian Nino <github@fnino.com>
2022-11-01 14:29:51 -0700

    Added product collection factory

--- Model/Api/Custom.php
+++ Model/Api/Custom.php
@@ -4,13 +4,21 @@ namespace Nailalliance\Colorcategory\Model\Api;
 
 use Exception;
 use Psr\Log\LoggerInterface;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 
 class Custom {
+    /** @var LoggerInterface */
     protected $logger;
 
-    public function __construct(LoggerInterface $logger)
-    {
+    /** @var CollectionFactory */
+    protected $_productCollectionFactory;
+
+    public function __construct(
+        LoggerInterface $logger, 
+        CollectionFactory $productCollectionFactory
+    ) {
         $this->logger = $logger;
+        $this->_productCollectionFactory = $productCollectionFactory;
     }
 
     /**
@@ -21,11 +29,29 @@ class Custom {
         $response = ['success' => false];
 
         try {
-            $response = ['success' => true, 'message' => $value];
+            $ids = explode(",", $value);
+            $collection = $this->_productCollectionFactory->create();
+            $collection->addAttributeToSelect('*');
+            $collection->addCategoriesFilter(['in' => $ids]);
+
+            // $response = ['success' => true, 'message' => $value];
+            $response = $this->parseCategoryProducts($collection);
         } catch (Exception $e) {
             $response = ['success' => false, 'message' => $e->getMessage()];
         }
 
         return json_encode($response); 
+    }
+
+    private function parseCategoryProducts($collection): array
+    {
+        $products = [];
+        foreach($collection as $product) {
+            $products[] = [
+                'name' => $product->getName(),
+                'url' => $product->getProductUrl()
+            ];
+        }
+        return $products;
     }
 }
